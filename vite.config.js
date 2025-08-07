fcb2b25ccf861c8fe4e5441ff080ec7871fa06dc
Olof Olsson <olof.olsson@snd.gu.se>
2025-08-07 16:49:12 +0200

    formatting fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
